1811519339e137379b830f1b53752b22a33f0798
William Filiberto May Couoh <ingsoftwaretx@gmail.com>
2023-12-06 21:28:45 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Buscador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.visorDatos = new System.Windows.Forms.DataGridView();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.visorDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // visorDatos
+            // 
+            this.visorDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.visorDatos.Location = new System.Drawing.Point(34, 52);
+            this.visorDatos.Name = "visorDatos";
+            this.visorDatos.Size = new System.Drawing.Size(445, 210);
+            this.visorDatos.TabIndex = 0;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBuscar.Location = new System.Drawing.Point(34, 13);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(174, 22);
+            this.txtBuscar.TabIndex = 1;
+            this.txtBuscar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtBuscar_MouseClick);
+            this.txtBuscar.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtBuscar.DoubleClick += new System.EventHandler(this.txtBuscar_DoubleClick);
+            this.txtBuscar.Enter += new System.EventHandler(this.txtBuscar_Enter);
+            this.txtBuscar.Leave += new System.EventHandler(this.txtBuscar_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 302);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.visorDatos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.visorDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView visorDatos;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buscador
+{
+    public partial class Form1 : Form
+    {
+        public string cCadenaBuscar = "Escribir búsqueda...";
+        public Form1()
+        {
+            InitializeComponent();
+            txtBuscar.Text = cCadenaBuscar;
+            txtBuscar.ForeColor = SystemColors.GrayText;
+            visorDatos.DataSource = ObtenerDataTable();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string textoBusqueda = txtBuscar.Text.ToLower();
+            DataRow[] filasFiltradas = ObtenerDataTable().Select($"Nombre LIKE '%{textoBusqueda}%'");
+            DataTable dataTableFiltrado = ObtenerDataTable().Clone();
+            foreach (DataRow fila in filasFiltradas)
+            {
+                dataTableFiltrado.ImportRow(fila);
+            }
+            visorDatos.DataSource = dataTableFiltrado;
+        }
+
+        private void txtBuscar_Enter(object sender, EventArgs e)
+        {
+            if (txtBuscar.Text.Equals(cCadenaBuscar))
+            {
+                visorDatos.DataSource = ObtenerDataTable();
+                txtBuscar.Text = "";
+                txtBuscar.ForeColor = SystemColors.ControlText;
+            }
+        }
+
+        private void txtBuscar_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtBuscar.Text))
+            {
+                txtBuscar.Text = cCadenaBuscar;
+                txtBuscar.ForeColor = SystemColors.GrayText;
+                visorDatos.DataSource = ObtenerDataTable();
+            }
+        }
+
+        private void txtBuscar_MouseClick(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void txtBuscar_DoubleClick(object sender, EventArgs e)
+        {
+        }
+
+        private DataTable ObtenerDataTable()
+        {
+            // Crear un DataTable con columnas
+            DataTable dataTable = new DataTable("EjemploDataTable");
+            dataTable.Columns.Add("ID", typeof(int));
+            dataTable.Columns.Add("Nombre", typeof(string));
+            dataTable.Columns.Add("Edad", typeof(int));
+
+            // Agregar filas al DataTable (puedes obtener estos datos de una base de datos, por ejemplo)
+            dataTable.Rows.Add(1, "Juan", 25);
+            dataTable.Rows.Add(2, "María", 30);
+            dataTable.Rows.Add(3, "Carlos", 22);
+            for (int i = 1; i <= 50; i++)
+            {
+                dataTable.Rows.Add(i, "Persona" + i, 20 + i);
+            }
+            return dataTable;
+        }
+
+        private DataTable BuscarElementoEnDataTable(string _cNombreBuscar, DataTable _dtDatos)
+        {
+            DataRow[] filasEncontradas = _dtDatos.Select($"Nombre = {QuitarDiacriticos(_cNombreBuscar)}");
+            DataTable dtResultado = new DataTable();
+            foreach (DataRow row in filasEncontradas)
+            {
+                dtResultado.ImportRow(row);
+            }
+            return dtResultado;
+        }
+
+        private string QuitarDiacriticos(string input)
+        {
+            string normalizedString = input.Normalize(NormalizationForm.FormD);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (char c in normalizedString)
+            {
+                if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                    stringBuilder.Append(c);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
