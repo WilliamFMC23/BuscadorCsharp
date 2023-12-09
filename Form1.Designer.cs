2c96567d2d4dd567deeb887367cbd4f9b6455110
William Filiberto May Couoh <ingsoftwaretx@gmail.com>
2023-12-09 14:29:13 -0600

    Se agrega importador de Registros en Excel

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,18 +29,23 @@ namespace Buscador
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.visorDatos = new System.Windows.Forms.DataGridView();
             this.txtBuscar = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.lblActalizar = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnImportar = new System.Windows.Forms.ToolStripButton();
             this.barraCarga = new System.Windows.Forms.ProgressBar();
+            this.lblOperacion = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.visorDatos)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // visorDatos
             // 
             this.visorDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.visorDatos.Location = new System.Drawing.Point(34, 52);
+            this.visorDatos.Location = new System.Drawing.Point(12, 70);
             this.visorDatos.Name = "visorDatos";
             this.visorDatos.Size = new System.Drawing.Size(445, 210);
             this.visorDatos.TabIndex = 0;
@@ -50,7 +55,7 @@ namespace Buscador
             // txtBuscar
             // 
             this.txtBuscar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBuscar.Location = new System.Drawing.Point(34, 13);
+            this.txtBuscar.Location = new System.Drawing.Point(12, 29);
             this.txtBuscar.Name = "txtBuscar";
             this.txtBuscar.Size = new System.Drawing.Size(174, 22);
             this.txtBuscar.TabIndex = 1;
@@ -74,18 +79,48 @@ namespace Buscador
             this.lblActalizar.Size = new System.Drawing.Size(0, 13);
             this.lblActalizar.TabIndex = 9;
             // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnImportar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(468, 25);
+            this.toolStrip1.TabIndex = 11;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnImportar
+            // 
+            this.btnImportar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnImportar.Image = ((System.Drawing.Image)(resources.GetObject("btnImportar.Image")));
+            this.btnImportar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnImportar.Name = "btnImportar";
+            this.btnImportar.Size = new System.Drawing.Size(23, 22);
+            this.btnImportar.Text = "Importar desde Excel";
+            this.btnImportar.Click += new System.EventHandler(this.btnImportar_Click);
+            // 
             // barraCarga
             // 
-            this.barraCarga.Location = new System.Drawing.Point(342, 13);
+            this.barraCarga.Location = new System.Drawing.Point(319, 30);
             this.barraCarga.Name = "barraCarga";
             this.barraCarga.Size = new System.Drawing.Size(137, 21);
             this.barraCarga.TabIndex = 10;
+            // 
+            // lblOperacion
+            // 
+            this.lblOperacion.AutoSize = true;
+            this.lblOperacion.Location = new System.Drawing.Point(319, 13);
+            this.lblOperacion.Name = "lblOperacion";
+            this.lblOperacion.Size = new System.Drawing.Size(0, 13);
+            this.lblOperacion.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 284);
+            this.ClientSize = new System.Drawing.Size(468, 314);
+            this.Controls.Add(this.lblOperacion);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.barraCarga);
             this.Controls.Add(this.lblActalizar);
             this.Controls.Add(this.label1);
@@ -94,6 +129,8 @@ namespace Buscador
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.visorDatos)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,7 +142,10 @@ namespace Buscador
         private System.Windows.Forms.TextBox txtBuscar;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblActalizar;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnImportar;
         private System.Windows.Forms.ProgressBar barraCarga;
+        private System.Windows.Forms.Label lblOperacion;
     }
 }
 
